5f3836b6fceffad990c88aebcf169b95c6a5bdbe
Claus Holst <clhols@gmail.com>
2018-07-05 19:38:38 +0200

    Build tools version 3.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 dependencies {
     compileOnly(gradleApi())
-    implementation("com.android.tools.build:gradle:3.1.1")
+    implementation("com.android.tools.build:gradle:3.1.3")
     implementation(kotlin("stdlib", "1.2.51"))
     implementation("org.json:json:20171018")
 }
