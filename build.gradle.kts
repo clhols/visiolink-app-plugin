ab83715d49ce350a0b83d9f8b7a32751c7d0f9f6
Claus Holst <clhols@gmail.com>
2018-07-05 19:42:14 +0200

    New JSON lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     compileOnly(gradleApi())
     implementation("com.android.tools.build:gradle:3.1.3")
     implementation(kotlin("stdlib", "1.2.51"))
-    implementation("org.json:json:20171018")
+    implementation("org.json:json:20180130")
 }
 
 repositories {
