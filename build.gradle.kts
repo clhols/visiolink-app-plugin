dbbd0860fef448e8c820acc72f7a7dfa25bd7a4d
Claus Holst <clhols@gmail.com>
2019-02-12 20:30:54 +0100

    Kotlin 1.3.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.2.71"
+    kotlin("jvm") version "1.3.21"
     `maven-publish`
     `java-gradle-plugin`
 }
@@ -7,7 +7,7 @@ plugins {
 dependencies {
     compileOnly(gradleApi())
     implementation("com.android.tools.build:gradle:3.2.0")
-    implementation(kotlin("stdlib", "1.2.71"))
+    implementation(kotlin("stdlib", "1.3.21"))
     implementation("org.json:json:20180130")
 }
 
